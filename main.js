5b9605a7ed51b260f32777f6f47449d116545506
Arman Arif <me@armanarif.com>
2025-05-03 23:48:16 +0600

    setup react to run on browser

---
+++ main.js
@@ -0,0 +1,9 @@
+const root = document.getElementById("root");
+
+const myElement = (
+  <div className="text-center">
+    <h1 className="text-[2rem] font-semibold">Hello, world!</h1>
+  </div>
+);
+
+ReactDOM.createRoot(root).render(myElement);
